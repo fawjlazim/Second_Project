1a49014efbe972f430f33afd9625b2d69559fca9
fawjlazim <mdfawjulazim.onlineid@gmail.com>
2024-09-04 12:43:12 +0600

    done

---
+++ js/script.js
@@ -0,0 +1,75 @@
+// let text = document.querySelectorAll(".text")
+// let arr = Array.from(text)
+
+// let colors = ["red","green","blue","pink","purple","yellow"]
+
+
+// arr.map(item=>{
+//     let typeText = item.innerHTML
+//     item.innerHTML = ""
+
+//     let count = -1
+
+//     function typejs(){
+//         count++
+//         item.innerHTML += `<span style="color:${colors[count]}">${typeText.charAt(count) }</span>` 
+//         if(count == typeText.length){
+//             item.innerHTML = ""
+//             count = -1
+
+//         }
+//     }
+
+//     let stop = setInterval(()=>{
+//     typejs()
+//     }, item.dataset.speed)
+    
+// })
+
+// let counter = document.querySelector(".counter")
+
+
+// let counterNumber = counter.innerHTML
+
+// counter.innerHTML = ""
+
+// let count = 0
+
+// function counterup(){
+//     count++
+//     counter.innerHTML = count
+
+//     if (count == counterNumber){
+//         clearInterval(stop)
+//     }
+// }
+
+
+// let stop = setInterval(()=>{
+//     counterup()
+// },500)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
